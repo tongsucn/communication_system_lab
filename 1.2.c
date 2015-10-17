4ba7fb6d0a32030656aa19b52a850a8a55015537
Tong Su <tong.su@rwth-aachen.de>
2015-10-17 09:59:31 +0200

    Add function: reading user name from keyboard input.

--- 1.2.c
+++ 1.2.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -31,5 +32,30 @@ typedef struct {
 // main
 int main(int argc, char *argv[])
 {
-     return 0;
+    // User name variable
+    #define NAME_LEN 256
+    char first_name[NAME_LEN];
+    char last_name[NAME_LEN];
+    int name_count;
+    uint8_t first_name_len;
+    uint8_t last_name_len;
+
+    // Read user's first name from keyboard
+    printf("First name: ");
+    name_count = 0;
+    while (name_count < NAME_LEN - 1 && (first_name[name_count] = getchar()) != '\n')
+        name_count++;
+    first_name[name_count] = '\0';
+    first_name_len = (uint8_t) strlen(first_name);
+
+    // Read user's last name from keyboard
+    printf("Last name: ");
+    name_count = 0;
+    while (name_count < NAME_LEN - 1 && (last_name[name_count] = getchar()) != '\n')
+        name_count++;
+    last_name[name_count] = '\0';
+    last_name_len = (uint8_t) strlen(first_name);
+
+
+    return 0;
 }
